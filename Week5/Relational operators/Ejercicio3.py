ff9fc76696b738394f4871b10e955000d7c2578e
Mario <kros.mg@hotmail.com>
2023-05-29 21:40:44 -0600

    Finished Relational operator Exercises

--- Week5/Relational operators/Ejercicio3.py
+++ Week5/Relational operators/Ejercicio3.py
@@ -1,3 +1,17 @@
 #Programa que calcule el interés de una cantidad si es mayor al 30%, sino informa el importe total.
 
-cantidad=0
+costo_objeto=float(input(f"Ingresa el costo del objeto: "))
+interes=float(input(f"Ingresa el porcentaje del interes: "))
+interes_solo=(0.01*interes)*costo_objeto
+total=interes_solo+costo_objeto
+
+
+if interes>30:
+    print(f"El interes es mayor a %30.")
+elif interes<=0:
+    print(f"Ingresa un porcentaje mayor a 0.")
+else:
+    print(f"__________________________________")
+    print(f"Total de articulos: ${costo_objeto}")
+    print(f"Total de Intereses: ${interes_solo}")
+    print(f"El importe total es: ${total}")
